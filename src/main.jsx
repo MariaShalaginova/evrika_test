5c861fe22ee6ad40df8f71c1b9619f54638112be
Maria Shalaginova <pro_m_@mail.ru>
2024-04-22 03:00:28 +0300

    убран роут

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+// import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={ <App /> }  />
-        </Routes>
-      </BrowserRouter>
+      {/* <BrowserRouter>
+        <Routes> */}
+         <App /> 
+        {/* </Routes>
+      </BrowserRouter> */}
   </React.StrictMode>,
 )
